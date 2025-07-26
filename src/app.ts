7ce40d42d8aa6799ef1cda62d3912ffaf47512a8
thanhch <thanhch@nal.vn>
2025-07-26 21:22:08 +0700

    Release version 1

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ class App {
 
     private initializeControllers(controllers: BaseController[]) {
         this.app.get("/", (request, response) => {
-            response.send("Application deploy prod Version 0 is running");
+            response.send("Application deploy prod Version 1 is running");
         });
         controllers.forEach((controller) => {
             this.app.use("/", controller.router);
